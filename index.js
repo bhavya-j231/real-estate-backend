0f18078ef92363ce15875c86127ac7b898ff4d0e
Bhavya_J <142712435+bhavya-j231@users.noreply.github.com>
2025-05-24 00:19:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,27 @@
- /*
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-
-// Routes
-app.get('/', (req, res) => {
-  res.send('Real Estate MVP Backend is running!!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
-
-*/
-
-
-
 const express = require('express');
 const app = express();
 const sequelize = require('./config/db');
 const listingRoutes = require('./routes/listingRoutes');
 const path = require('path');
-
 const cors = require('cors');
-app.use(cors());
 
+app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/listings', listingRoutes);
 
-sequelize.sync().then(() => {
-  app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+// Add this to test server alive easily
+app.get('/', (req, res) => {
+  res.send('Server is running!');
 });
 
-
+sequelize.sync()
+  .then(() => {
+    console.log('Sequelize sync successful');
+    app.listen(3000, () => {
+      console.log('Server running on http://localhost:3000');
+    });
+  })
+  .catch(err => {
+    console.error('Sequelize sync error:', err);
+  });
